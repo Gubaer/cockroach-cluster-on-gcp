50a5777854daaf05e9f3f5fb362abf50773f7e6a
Gubaer <karl.guggisberg@guggis.ch>
2017-10-30 10:22:46 +0100

    generate node-keys.sv

--- osm-poc/generate-csv-files.py
+++ osm-poc/generate-csv-files.py
@@ -1,34 +1,49 @@
 #
 # generates the CVS files to be imported in the cockroach db
-#from imposm.parser import OSMParserfrom imposm.parser import OSMParser
+#
 from imposm.parser import OSMParser
 import csv
+import os
 
 DEFAULT_IMPORT_FILE='switzerland-latest.osm.pbf'
 
-
 class CsvExportHandlerForNodes(object):
     """Generates the CSV file to import nodes"""
-    writer = None
+    nodes_writer = None
+    node_keys_writer = None
 
-    def __init__(self, writer):
+    def __init__(self, nodes_writer, node_keys_writer):
         """Initializer
         
         Keyword arguments:
-        writer -- a csv writer. Must not be None.
+        nodes_writer -- a csv writer, target for node enries. Must not be None.
+        node_keys_writer -- a csv writer, target for node key entries. Must not be None
         """
-        assert writer != None
-        self.writer = writer
+        assert nodes_writer != None
+        assert node_keys_writer != None
+        self.nodes_writer = nodes_writer
+        self.node_keys_writer = node_keys_writer
 
     def nodes(self, nodes):
         for osmid, tags, coords in nodes:
             (lat, lon) = coords
-            self.writer.writerow([osmid, 1, lat, lon, 1, True])
+            self.nodes_writer.writerow([osmid, 1, lat, lon, 1, True])
+            for key, value in tags.items():
+                self.node_keys_writer.writerow(
+                    [osmid, key.encode('utf-8'), value.encode('utf-8')]
+                )
 
-with open("nodes.csv", "wb") as csvfile:
-    nodewriter = csv.writer(csvfile, delimiter=",")
-    handler = CsvExportHandlerForNodes(nodewriter)
-    parser = OSMParser(concurrency=10, nodes_callback=handler.nodes)
-    parser.parse(DEFAULT_IMPORT_FILE)
+if not os.path.isdir("csv-files"):
+    os.mkdir("csv-files")
+
+with open("csv-files/nodes.csv", "wb") as nodes_csv:
+    with open("csv-files/node-keys.csv", "wb") as node_keys_csv:
+        print("Writing nodes to '{file}' ...".format(file="csv-files/nodes.csv"))
+        print("Writing node keys to '{file}' ...".format(file="csv-files/node-keys.csv"))
+        nodes_writer = csv.writer(nodes_csv, delimiter=",")
+        node_keys_writer = csv.writer(node_keys_csv, delimiter=",")
+        handler = CsvExportHandlerForNodes(nodes_writer, node_keys_writer)
+        parser = OSMParser(concurrency=10, nodes_callback=handler.nodes)
+        parser.parse(DEFAULT_IMPORT_FILE)
 
 
